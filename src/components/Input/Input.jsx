ceebce0f79ee456ebd5aac52feb62bc2a57d820f
RolandoPranataAR <rolandopranata@gmail.com>
2023-02-04 21:10:18 +0700

    chore: turn off input autocomplete

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -10,6 +10,7 @@ export const Input = forwardRef(({ label, id, type, placeholder, ...rest }, ref)
         ref={ref}
         placeholder={placeholder}
         className="block w-full p-3 bg-transparent outline-none ring-1 ring-gray-400 rounded-xl text-primary focus:ring-primary"
+        autoComplete="off"
         required
         {...rest}
       />
